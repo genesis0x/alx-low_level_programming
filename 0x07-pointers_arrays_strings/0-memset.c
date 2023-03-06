5be382c50f612f98e94564fc775dea913dd9d9b8
genesis0x <ythamza19@gmail.com>
2023-03-06 06:42:40 -0800

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,5 +8,14 @@ char *_memset(char *s, char b, unsigned int n)
 		s[i] = b;
 		i++;
 	}
+	s[i] = '\0';
 	return (s);
 }
+/*
+#include <stdio.h>
+int main(void)
+{
+	char s[5] = "hello";
+	printf("%s\n", _memset(s, '0', 5));
+	return 0;
+}*/
